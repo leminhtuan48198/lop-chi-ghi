e4d7624466d19ffa135bdcf45f43bfcbdace166c
Tuan le <leminhtuan48198@gmail.com>
2022-08-10 11:50:59 +0700

    add file chi ghi

---
+++ src/a/Student.java
@@ -0,0 +1,17 @@
+package a;
+
+public class Student {
+    private String name;
+    private String classes;
+    Student(){
+        name="John";
+        classes="C02";
+    }
+     public void setName(String name){
+        this.name=name;
+    }
+    public void setClasses(String classes){
+        this.classes=classes;
+    }
+
+}
---
+++ src/a/Test.java
@@ -0,0 +1,9 @@
+package a;
+
+public class Test {
+    public static void main(String[] args) {
+        Student tuan =new Student();
+        tuan.setName("tuan");
+        tuan.setClasses("C0622i1");
+    }
+}
